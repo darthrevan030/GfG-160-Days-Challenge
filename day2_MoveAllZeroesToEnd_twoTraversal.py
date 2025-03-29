5f04b49be8aff150036e831ddd65d3f32c24191b
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 13:30:22 +0800

    Renamed and added explanation of thought process Two Traversal Method for Day 2 Move All Zeros To End

--- day2_MoveAllZeroesToEnd_twoTraversal.py
+++ day2_MoveAllZeroesToEnd_twoTraversal.py
@@ -4,6 +4,18 @@ Your task is to move all the zeros in the array to the right end while maintaini
 The operation must be performed in place, meaning you should not use extra space for another array.
 '''
 
+'''
+First Traversal: Shift non-zero elements
+Traverse the array and maintain the count of non-zero elements. This count is initialized with 0 and keeps track 
+of where the next non-zero element should be placed in the array. If the element is non-zero, place it at arr[count] 
+and increment count by 1. After traversing all the elements, all non-zero elements will be shifted 
+to the front while maintaining their original order.
+
+Second Traversal: Fill remaining positions with zeros
+After the first traversal, all non-zero elements will be at the start of the array and count will store the index 
+where the first zero should be placed. Iterate from count to the end of array and fill all indices with 0.
+'''
+
 class Solution:
 	def pushZerosToEnd(self,arr):
 		# code here
