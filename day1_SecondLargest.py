7ef1a34d099aaeefc992836820af1899b795c836
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-29 11:29:45 +0800

    Added thought process for Native approach

--- day1_SecondLargest.py
+++ day1_SecondLargest.py
@@ -5,6 +5,13 @@ If the second largest element doesn't exist then return -1.
 Note: The second largest element should not be equal to the largest element
 '''
 
+'''
+The idea is to sort the array in non-decreasing order. Now, we know that the largest element will be at index n - 1. 
+So, starting from index (n - 2), traverse the remaining array in reverse order. As soon as we encounter an element which 
+is not equal to the largest element, return it as the second largest element in the array. 
+If all the elements are equal to the largest element, return -1.
+'''
+
 #User function Template for python3
 class Solution:
     def getSecondLargest(self, arr):
