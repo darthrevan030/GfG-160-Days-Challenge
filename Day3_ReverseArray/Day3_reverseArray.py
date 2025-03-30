74786d24a1245c9269d1d0abec554e5389035f04
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-30 14:56:13 +0800

    Add main execution block to reverseArray function for testing

--- Day3_ReverseArray/Day3_reverseArray.py
+++ Day3_ReverseArray/Day3_reverseArray.py
@@ -2,4 +2,14 @@
 You are given an array of integers arr[]. Your task is to reverse the given array.
 Note: Modify the array in place.'''
 
+class Solution:
+
 def reverseArray(self, arr):
+
+if __name__ == "__main__":
+	arr = [1, 2, 0, 4, 3, 0, 5, 5, 0, 3, 2]
+	solution = Solution()
+	solution.reverseArray(arr)
+	
+	for num in arr:
+		print(num, end = " ")
