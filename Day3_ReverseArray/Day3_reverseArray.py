f96fd98f2c2827b7e1c8bb1429e4c3f7de717a63
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-30 15:02:39 +0800

    Fully accepted Solution to Day 3 reverseArray

--- Day3_ReverseArray/Day3_reverseArray.py
+++ Day3_ReverseArray/Day3_reverseArray.py
@@ -3,11 +3,15 @@ You are given an array of integers arr[]. Your task is to reverse the given arra
 Note: Modify the array in place.'''
 
 class Solution:
-
-def reverseArray(self, arr):
+	def reverseArray(self, arr):
+		# code here
+		n = len(arr)
+		
+		for i in range(n//2):
+			arr[i], arr[n-1-i] = arr[n-1-i], arr[i]
 
 if __name__ == "__main__":
-	arr = [1, 2, 0, 4, 3, 0, 5, 5, 0, 3, 2]
+	arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	solution = Solution()
 	solution.reverseArray(arr)
 	
