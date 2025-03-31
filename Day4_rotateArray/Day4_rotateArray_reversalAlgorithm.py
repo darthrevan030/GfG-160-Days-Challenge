7a5bf917d28a65d7400b9313e4bc06386a5e9b20
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-31 10:37:08 +0800

    Implement Reversal Algorithm for Day 4  rotateArray with helper function for reversing subarrays

--- Day4_rotateArray/Day4_rotateArray_reversalAlgorithm.py
+++ Day4_rotateArray/Day4_rotateArray_reversalAlgorithm.py
@@ -33,6 +33,28 @@ Space complexity: O(1)
 import math
 
 def rotateArr(arr, d):
+    # helper function to reverse an array between 2 indexes
+    def reverse(arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+    
+    n = len(arr)
+
+    d %= n # handle the case here d > n
+
+    # reverse first d elements
+    reverse(arr, 0, d -1)
+
+    # reverse remaining n - d elements
+    reverse(arr, d, n - 1)
+
+    # reverse the entire array
+    reverse(arr, 0, n -1)
+
+
+
     
 
 if __name__ == "__main__":
