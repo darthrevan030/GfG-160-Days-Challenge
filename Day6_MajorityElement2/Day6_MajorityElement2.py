7b9d4dd2aab56e972aa24d82b75d1bd26e777a27
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-01 22:29:57 +0800

    Day 6 Majority Element using Dictionary/Hash Map

--- Day6_MajorityElement2/Day6_MajorityElement2.py
+++ Day6_MajorityElement2/Day6_MajorityElement2.py
@@ -8,4 +8,24 @@ Note: The answer should be returned in an increasing format.
 
 class Solution:
     def findMajority(self, arr):
-        pass
+        n = len(arr)
+
+        count = {item:arr.count(item) for item in arr}
+        result = []
+        
+        for key, value in count.items():
+            if value > n // 3:
+                result.append(key)
+        
+        return result
+
+if __name__ == "__main__":
+    arr = [2, 2, 3, 1, 3, 2, 1, 1]
+    solution = Solution()
+    result = solution.findMajority(arr)
+    print(result)
+    
+    arr2 = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]
+    result2 = solution.findMajority(arr2)
+    print(result2)
+        
