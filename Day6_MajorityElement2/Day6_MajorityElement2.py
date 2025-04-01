8b8ac8c73349d5a8041156c8ca3453b97896b186
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-01 22:16:23 +0800

    Day 6 Majority Element II

---
+++ Day6_MajorityElement2/Day6_MajorityElement2.py
@@ -0,0 +1,11 @@
+'''
+You are given an array of integer arr[] where each number represents a vote to a candidate. 
+Return the candidates that have votes greater than one-third of the total votes, If there's 
+not a majority vote, return an empty array. 
+
+Note: The answer should be returned in an increasing format.
+'''
+
+class Solution:
+    def findMajority(self, arr):
+        pass
