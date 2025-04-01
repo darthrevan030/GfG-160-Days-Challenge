72051f48b79f054882de403206ff82b7a8d604de
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-01 21:23:47 +0800

    Enhance docstring in nextPermutation.py to clarify the approach for finding the next permutation

--- Day5_NextPermutation/Day5_nextPermuatition.py
+++ Day5_NextPermutation/Day5_nextPermuatition.py
@@ -4,6 +4,13 @@ into the lexicographically next greater permutation. If no such permutation exis
 possible order (i.e., sorted in ascending order). 
 
 Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+
+Approach: 
+    To get the next permutation we change the number in a position which is as right as possible. The first number to be 
+    moved is the rightmost number smaller than its next. The number to come in-place is the rightmost greater number on 
+    right side of the pivot. Each permutation (except the very first) has an increasing suffix. Now if we change the pattern
+    from the pivot point (where the increasing suffix breaks) to its next possible lexicographic representation we will get 
+    the next greater permutation.
 '''
 def nextPermutation(self, arr):
 if __name__ == "__main__":
