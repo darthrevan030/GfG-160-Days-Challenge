c37ba86b209ddf51f2923f1b5170d49ba58a529d
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-01 21:19:01 +0800

    Add main function to demonstrate nextPermutation usage

--- Day5_NextPermutation/Day5_nextPermuatition.py
+++ Day5_NextPermutation/Day5_nextPermuatition.py
@@ -6,3 +6,7 @@ possible order (i.e., sorted in ascending order).
 Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
 '''
 def nextPermutation(self, arr):
+if __name__ == "__main__":
+    arr = [2, 4, 1, 7, 5, 0]
+    nextPermutation(arr)
+    print(" ".join(map(str, arr)))
