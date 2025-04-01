41a1f7e08e0f1a33b02afeef530ec0ebce214c2c
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-01 21:12:26 +0800

    Day 5 Next Permutation

---
+++ Day5_NextPermutation/Day5_nextPermuatition.py
@@ -0,0 +1,8 @@
+'''
+Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers 
+into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest 
+possible order (i.e., sorted in ascending order). 
+
+Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+'''
+def nextPermutation(self, arr):
