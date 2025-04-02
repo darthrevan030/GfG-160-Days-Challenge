000f80579da9a7f4f37046338bf3b2820a203ed5
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-02 11:04:06 +0800

    Day 3 Reverse Array Using Two Pointers in Python

---
+++ Day3_ReverseArray/Day3_reverseArray_TwoPointers.py
@@ -0,0 +1,33 @@
+# Python Program to reverse an array using Two Pointers
+
+'''
+You are given an array of integers arr[]. Your task is to reverse the given array.
+Note: Modify the array in place.
+'''
+
+# function to reverse an array
+def reverseArray(arr):
+    
+    # Initialize left to the beginning and right to the end
+    left = 0
+    right = len(arr) - 1
+
+    # Iterate till left is less than right
+    while left < right:
+        
+        # Swap the elements at left and right position
+        arr[left], arr[right] = arr[right], arr[left]
+
+        # Increment the left pointer
+        left += 1
+
+        # Decrement the right pointer
+        right -= 1
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
