da2b100f0ed9017296974bea386312faddbd793d
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-02 11:03:51 +0800

    Day 3 Reverse Array Using Two Pointers in C

---
+++ Day3_ReverseArray/Day3_reverseArray_TwoPointers.c
@@ -0,0 +1,48 @@
+// C Program to reverse an array using Two Pointers
+
+/*
+You are given an array of integers arr[]. Your task is to reverse the given array.
+Note: Modify the array in place.
+*/
+
+
+#include <stdio.h>
+
+// Function to swap two numbers
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// function to reverse an array
+void reverseArray(int arr[], int n) {
+    
+    // Initialize left to the beginning and right to the end
+    int left = 0, right = n - 1;
+
+    // Iterate till left is less than right
+    while (left < right) {
+        
+        // Swap the elements at left and right position
+        swap(&arr[left], &arr[right]);
+
+        // Increment the left pointer
+        left++;
+
+        // Decrement the right pointer
+        right--;
+    }
+}
+
+int main() {
+    int arr[] = { 1, 4, 3, 2, 6, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, n);
+  
+    for (int i = 0; i < n; i++) 
+        printf("%d ", arr[i]);
+    
+    return 0;
+}
