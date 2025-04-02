1bfb822252bb897590caacb27f0b8b60966ebcf6
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-02 11:07:09 +0800

    Day 2 Move All Zeros to End Using Two Traversal in C

---
+++ Day2_MoveAllZerosToEnd/day2_MoveAllZeroesToEnd_twoTraversal.c
@@ -0,0 +1,32 @@
+// C Program to move all zeros to end using two traversals
+
+#include <stdio.h>
+
+// Function which pushes all zeros to end 
+void pushZerosToEnd(int* arr, int n) {
+    
+    // Count of non-zero elements
+    int count = 0;  
+
+    // If the element is non-zero, replace the element at
+    // index 'count' with this element and increment count
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0)
+            arr[count++] = arr[i];
+    }
+
+    // Now all non-zero elements have been shifted to
+    // the front. Make all elements 0 from count to end.
+    while (count < n)
+        arr[count++] = 0;
+}
+
+int main() {
+    int arr[] = {1, 2, 0, 4, 3, 0, 5, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    pushZerosToEnd(arr, size);
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
