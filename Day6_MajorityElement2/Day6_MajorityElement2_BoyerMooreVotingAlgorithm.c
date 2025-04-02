ce3f6535df921847bff91131c3d7a4504257b5e3
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-02 10:35:59 +0800

    Optimal Answer for Day 6 Majority Element using Boyer-Moore Voting Algorithm in C

---
+++ Day6_MajorityElement2/Day6_MajorityElement2_BoyerMooreVotingAlgorithm.c
@@ -0,0 +1,103 @@
+/*
+You are given an array of integer arr[] where each number represents a vote to a candidate. 
+Return the candidates that have votes greater than one-third of the total votes, If there's 
+not a majority vote, return an empty array. 
+
+Note: The answer should be returned in an increasing format.
+
+The idea is based on the observation that there can be at most two majority elements, which appear more 
+than n/3 times. so we can use Boyer-Moore’s Voting algorithm. As we iterate the array, We identify 
+potential majority elements by keeping track of two candidates and their respective counts.
+
+Steps:
+    - Initialize two variables ele1 = -1 and ele2 = -1, for candidates and two variables cnt1 = 0 and cnt2 = 0, for counting.
+    - In each iteration,
+        - If an element is equal to any candidate, update that candidate's count.
+        - If count of a candidate reaches zero then replace that candidate with current element.
+        - If neither candidate matches and both counts are non zero, decrement the counts.
+    - After this, in second pass we check if the chosen candidates appear more than n/3 times in the array. 
+    - If they do then include them in result array.
+    - Since any element than appears more than floor(n/3) times, will dominate over elements that appear less frequently.
+    - Whenever we encounter a different element, we decrement the count of both the candidates. This maintains at most 
+    two candidates in the array.
+
+*/
+
+// C program for finding the majority element in an array
+// using Moore’s Voting algorithm
+
+#include <stdio.h>
+#include <limits.h>
+
+void findMajority(int arr[], int n, int *res, int *resSize) {
+    
+    // Initialize two candidates and their counts
+    int ele1 = -1, ele2 = -1, cnt1 = 0, cnt2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        int ele = arr[i];
+
+        // Increment count for candidate 1
+        if (ele1 == ele) {
+            cnt1++;
+        }
+        
+        // Increment count for candidate 2
+        else if (ele2 == ele) {
+            cnt2++;
+        }
+        
+        // New candidate 1 if count is zero
+        else if (cnt1 == 0) {
+            ele1 = ele;
+            cnt1++;
+        }
+        
+        // New candidate 2 if count is zero
+        else if (cnt2 == 0) {
+            ele2 = ele;
+            cnt2++;
+        }
+        
+        // Decrease counts if neither candidate
+        else {
+            cnt1--;
+            cnt2--;
+        }
+    }
+
+    cnt1 = 0;
+    cnt2 = 0;
+
+    // Count the occurrences of candidates
+    for (int i = 0; i < n; i++) {
+        int ele = arr[i];
+        if (ele1 == ele) cnt1++;
+        if (ele2 == ele) cnt2++;
+    }
+
+    // Add to result if they are majority elements
+    *resSize = 0;
+    if (cnt1 > n / 3) res[(*resSize)++] = ele1;
+    if (cnt2 > n / 3 && ele1 != ele2) res[(*resSize)++] = ele2;
+
+    // Sort the result if there are two elements
+    if (*resSize == 2 && res[0] > res[1]) {
+        int temp = res[0];
+        res[0] = res[1];
+        res[1] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {2, 2, 3, 1, 3, 2, 1, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int res[2]; 
+    int resSize;
+
+    findMajority(arr, n, res, &resSize);
+    for (int i = 0; i < resSize; i++) {
+        printf("%d ", res[i]);
+    }
+    return 0;
+}
