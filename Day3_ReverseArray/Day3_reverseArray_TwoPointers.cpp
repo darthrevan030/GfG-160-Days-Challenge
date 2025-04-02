d909aa54fc29830be66ba5189cc8f023494fb469
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-02 11:03:57 +0800

    Day 3 Reverse Array Using Two Pointers in C++

---
+++ Day3_ReverseArray/Day3_reverseArray_TwoPointers.cpp
@@ -0,0 +1,40 @@
+/*
+You are given an array of integers arr[]. Your task is to reverse the given array.
+Note: Modify the array in place.
+*/
+
+// C++ Program to reverse an array using Two Pointers
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// function to reverse an array
+void reverseArray(vector<int> &arr) {
+
+    // Initialize left to the beginning and right to the end
+    int left = 0, right = arr.size() - 1;
+
+    // Iterate till left is less than right
+    while(left < right) {
+
+        // Swap the elements at left and right position
+        swap(arr[left], arr[right]);
+
+        // Increment the left pointer
+        left++;
+
+        // Decrement the right pointer
+        right--;
+    }
+}
+
+int main() {
+    vector<int> arr = { 1, 4, 3, 2, 6, 5 };
+
+    reverseArray(arr);
+
+    for(int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
