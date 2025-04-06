586aed137c57d920b9e96a2b0642f5f06eb83be5
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-06 14:43:15 +0800

    Add Main function for test cases

--- Day10_KadanesAlgorithm/Day10_KadanesAlgorithm.py
+++ Day10_KadanesAlgorithm/Day10_KadanesAlgorithm.py
@@ -3,3 +3,13 @@ Given an array arr[]. You need to find and return the maximum sum from all the s
 '''
 
 def maxSubArraySum(arr):
+    
+if __name__ == "__main__":
+    arr = [2, 3, -8, 7, -1, 2, 3]
+    print(maxSubArraySum(arr))
+
+    arr2 = [-2, -4]
+    print(maxSubArraySum(arr2))
+
+    arr3 = [5, 4, 1, 7, 8]
+    print(maxSubArraySum(arr3))
