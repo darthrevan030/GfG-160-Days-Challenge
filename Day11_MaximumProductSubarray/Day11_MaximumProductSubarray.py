244663384f74823640602a26acda77a518e760ad
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-07 11:41:13 +0800

    Day 11 Maximum Product Subarray

---
+++ Day11_MaximumProductSubarray/Day11_MaximumProductSubarray.py
@@ -0,0 +1,17 @@
+'''
+Given an array arr[] that contains positive and negative integers (may contain 0 as well). 
+Find the maximum product that we can get in a sub-array of arr[].
+
+Note: It is guaranteed that the output fits in a 32-bit integer.
+'''
+
+def maxProduct(arr):
+if __name__ == "__main__":
+    arr = [-2, 6, -3, -10, 0, 2]
+    print(maxProduct(arr))
+
+    arr2 = [-1, -3, -10, 0, 6]
+    print(maxProduct(arr2))
+
+    arr3 = [2, 3, 4] 
+    print(maxProduct(arr3))
