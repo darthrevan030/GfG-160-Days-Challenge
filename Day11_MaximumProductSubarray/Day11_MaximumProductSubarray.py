9f0113a2aca790bd36115036cf6b14d30e1ac1ef
darth.revan.030 <smartsamarth03@gmail.com>
2025-04-07 11:58:41 +0800

    Day 11 Max Product Subarray 1st Attempt Using Kadane's Algorithm

--- Day11_MaximumProductSubarray/Day11_MaximumProductSubarray.py
+++ Day11_MaximumProductSubarray/Day11_MaximumProductSubarray.py
@@ -6,6 +6,18 @@ Note: It is guaranteed that the output fits in a 32-bit integer.
 '''
 
 def maxProduct(arr):
+    n = len(arr)
+
+    result = arr[0]
+    maxEnding = arr[0]
+
+    for i in range(1, n):
+        maxEnding = max(maxEnding * arr[i], arr[i])
+
+        result = max(maxEnding, result)
+
+    return result
+
 if __name__ == "__main__":
     arr = [-2, 6, -3, -10, 0, 2]
     print(maxProduct(arr))
